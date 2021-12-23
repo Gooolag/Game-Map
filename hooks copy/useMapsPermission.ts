05473c96c49a946713848371ce8972abd77a1e71
arcinston <arushkurundodi@gmail.com>
2021-12-23 18:06:34 +0530

    added useMap hook Co-authored-by: Om Gupta <OmGuptaIND@users.noreply.github.com>

---
+++ hooks copy/useMapsPermission.ts
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+
+type useMapsPermissionTypes = {
+  status: String;
+  location: Location.LocationObject | null;
+  errorMsg: String;
+};
+
+const useMapsPermission = (): useMapsPermissionTypes => {
+  const [status, setStatus] = useState<String>("");
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<String>("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.getPermissionsAsync();
+      console.log(status);
+
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+    })();
+  }, []);
+
+  return { status, location, errorMsg };
+};
+
+export default useMapsPermission;
+
+const styles = StyleSheet.create({});
--- utils/types.ts
+++ utils/types.ts
@@ -1,7 +1,9 @@
+import * as Location from "expo-location";
+
 export type navigatorTabsType = {
-    id: Number,
-    label : String,
-    icon: String
+  id: Number;
+  label: String;
+  icon: String;
 };
 
 export type bottomTabIndexType = Number | any | BigInt;
