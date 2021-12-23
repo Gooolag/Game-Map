08b6f3ffc9a602c010765ca3740f97a5a91dfc98
sleepinGiant <shukla.ayush0203@gmail.com>
2021-12-24 00:07:41 +0530

    Login page basic design

--- screens/Login/Login.Screen.tsx
+++ screens/Login/Login.Screen.tsx
@@ -1,71 +1,104 @@
-// import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, FC } from "react";
+import { StatusBar } from "expo-status-bar";
 import {
     StyleSheet,
     Text,
     View,
-    Image,
     TextInput,
-    Button,
     TouchableOpacity,
 } from "react-native";
 
-export default function App() {
+interface Props {}
+
+const Login: FC<Props> = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     return (
         <View style={styles.container}>
-            <Text>THis is login page</Text>
+            <View>
+                <Text style={styles.title}>NuggahFinder</Text>
+            </View>
+
+            <StatusBar style="auto" />
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Email"
+                    placeholderTextColor="#282C35"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Password"
+                    placeholderTextColor="#282C35"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+            </View>
+
+            <TouchableOpacity>
+                <Text style={styles.forgotBtn}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
     },
 
-    image: {
-        marginBottom: 40,
+    title: {
+        fontSize: 30,
+        marginTop: -150,
+        padding: 10,
     },
 
     inputView: {
-        backgroundColor: "#FFC0CB",
-        borderRadius: 30,
+        backgroundColor: "#D4D5D6",
+        borderRadius: 25,
         width: "70%",
         height: 45,
         marginBottom: 20,
-
         alignItems: "center",
     },
 
-    TextInput: {
+    textInput: {
         height: 50,
         flex: 1,
         padding: 10,
         marginLeft: 20,
+        justifyContent: "center",
     },
 
-    forgot_button: {
+    forgotBtn: {
         height: 30,
         marginBottom: 30,
     },
 
     loginBtn: {
-        width: "80%",
+        width: "30%",
         borderRadius: 25,
-        height: 50,
+        height: 40,
         alignItems: "center",
         justifyContent: "center",
         marginTop: 40,
-        backgroundColor: "#FF1493",
+        backgroundColor: "#D4D5D6",
     },
 
     loginText: {
-        fontSize: 20,
+        fontSize: 19,
     },
 });
+
+export default Login;
